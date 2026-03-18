c4132b4cbd48ee8e8defaa9ee359a9e37c93f572
Venkatesh <venkatesh1211559@gmail.com>
2026-03-18 10:10:30 +0530

    Day 15: GitHub Actions CI/CD pipeline

--- docker-ec2/main.tf
+++ docker-ec2/main.tf
@@ -12,7 +12,14 @@ resource "aws_security_group" "docker_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["106.51.27.68/32"]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
--- terraform/main.tf
+++ terraform/main.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "main" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["106.51.24.57/32"]
+    cidr_blocks = ["106.51.27.62/32"]
   }
 
   egress  {
