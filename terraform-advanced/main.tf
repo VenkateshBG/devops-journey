e5a9cde3d50ff326bdb084f6835c063bc9c98b7b
Venkatesh <venkatesh1211559@gmail.com>
2026-03-30 17:29:32 +0530

    Month 2: Terraform workspaces

--- terraform-advanced/main.tf
+++ terraform-advanced/main.tf
@@ -12,21 +12,8 @@ provider "aws" {
   region = var.region
 }
 
-# Create 3 S3 buckets using same module!
-module "s3_dev" {
+module "s3" {
   source      = "./modules/s3"
-  bucket_name = "venkatesh-dev-bucket-2024"
-  environment = "dev"
-}
-
-module "s3_staging" {
-  source      = "./modules/s3"
-  bucket_name = "venkatesh-staging-bucket-2024"
-  environment = "staging"
-}
-
-module "s3_prod" {
-  source      = "./modules/s3"
-  bucket_name = "venkatesh-prod-bucket-2024"
-  environment = "prod"
+  bucket_name = "venkatesh-${terraform.workspace}-bucket-2024"
+  environment = terraform.workspace
 }
--- terraform-advanced/outputs.tf
+++ terraform-advanced/outputs.tf
@@ -1,11 +1,7 @@
-output "dev_bucket" {
-  value = module.s3_dev.bucket_name
+output "bucket_name" {
+  value = module.s3.bucket_name
 }
 
-output "staging_bucket" {
-  value = module.s3_staging.bucket_name
-}
-
-output "prod_bucket" {
-  value = module.s3_prod.bucket_name
+output "environment" {
+  value = terraform.workspace
 }
